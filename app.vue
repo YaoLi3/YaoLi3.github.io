ca4a0e8c97c11386ce3e82b7f9389c511cd5d670
Yao <liyaoo6o@gmail.com>
2022-06-14 13:56:55 +0800

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header>
+      <p>header</p>
+    </header>
+    <hr>
+    <footer>
+      <p>Contact: liyao1@genomics.cn; </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
