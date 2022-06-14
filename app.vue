c77d6b134c377421e9fe1d4fc06951ed561d78d8
Yao <liyaoo6o@gmail.com>
2022-06-14 14:13:12 +0800

    el starter app.vue

--- app.vue
+++ app.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <header>
-      <p>header</p>
-    </header>
-    <hr>
-    <footer>
-      <p>Contact: liyao1@genomics.cn; </p>
-    </footer>
+    <img src="./assets/logo.png">
+    <div>
+      <el-button @click="startHacking">Start</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-
+export default {
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
+}
 </script>
 
 <style>
